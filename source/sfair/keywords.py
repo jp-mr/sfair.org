41d941fa22fe62bab1fed2f7a566920f9f75dfbb
Michel <michel.rodrigues86@yahoo.com.br>
2016-08-24 16:05:47 -0300

    update

--- source/sfair/keywords.py
+++ source/sfair/keywords.py
@@ -3,7 +3,7 @@ import os
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 def keys():
-    keys = open(os.path.join(os.path.dirname(BASE_DIR), "keywords"), 'r').read()
+    keys = open(os.path.join(os.path.dirname(BASE_DIR), ".keywords.src"), 'r').read()
     keywords = keys.split("\n")
     keywords.pop()
     return keywords
